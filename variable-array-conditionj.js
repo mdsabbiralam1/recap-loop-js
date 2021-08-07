e2fcbcab47c15388800c4b59a89a7a1f187fbab2
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-08 01:25:12 +0600

    add variable-array-conditionj.js file

---
+++ variable-array-conditionj.js
@@ -0,0 +1,20 @@
+var bottleColor = "yellow";
+var waterQuantity = 1;
+var isfull = false;
+
+// array
+var iteams = ["mouse, bottle, speaker, book, glass, charger, multiflag"];
+items.indexOf('glass');
+iteams.push('wallcloth');
+iteams.pop();
+
+// conditional
+if (iteams.length >= 4) {
+    console.log("you have too many stuff on your desk.")
+}
+else if (iteams.hight > 564) {
+    console.log("great mama...!")
+}
+else {
+    console.log('bash khao')
+}
